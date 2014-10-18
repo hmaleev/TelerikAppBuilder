036d5e459bcfb1e0af158768a334ddf7aa862c22
Hristo Maleev <hmaleev@gmail.com>
2014-10-18 10:20:04 +0000

    updated javascript and styles

---
+++ DestroyerGame/scripts/main.js
@@ -0,0 +1,49 @@
+var newGameBtn = $("#newGameBtn");
+var highScoresBtn = $("#highScoresBtn");
+var settingsBtn = $("#settingsBtn");
+
+var settingsView = $("#settingsView");
+var highScoresView = $("#localHighScoresView");
+var newGameView = $("#gameView");
+var homepage =$("#homepage");
+
+homepage.show();
+newGameView.hide();
+settingsView.hide();
+highScoresView.hide();
+
+newGameBtn.click(function (){
+    
+    homepage.hide();
+    newGameView.show();
+    settingsView.hide();
+    highScoresView.hide();
+});
+
+highScoresBtn.click(function (){
+ 
+    homepage.hide();
+    newGameView.hide();
+    settingsView.hide();
+    highScoresView.show();
+});
+
+settingsBtn.click(function (){
+   // alert('New game started');
+    homepage.hide();
+    newGameView.hide();
+    settingsView.show();
+    highScoresView.hide();
+})
+
+document.addEventListener("backbutton", onBackKeyDown, false);
+
+function onBackKeyDown() {
+    // Handle the back button
+    
+    homepage.show();
+    newGameView.hide();
+    settingsView.hide();
+    highScoresView.hide();
+
+}
